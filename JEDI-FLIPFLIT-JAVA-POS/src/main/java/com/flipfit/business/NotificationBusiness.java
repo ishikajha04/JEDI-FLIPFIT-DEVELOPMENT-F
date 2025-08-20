8b257b7aeb21db04d07a1d74a641b9a417043a1f
sukhmani kaur <ksukhmani0910@gmail.com>
2025-08-20 10:24:40 +0530

    Business changes

--- JEDI-FLIPFLIT-JAVA-POS/src/main/java/com/flipfit/business/NotificationBusiness.java
+++
@@ -1,7 +0,0 @@
-package com.flipfit.business;
-
-public class NotificationBusiness {
-    public void pushNotification(int userid,String message){
-        System.out.println("Pushing notification for id " + userid);
-    }
-}
